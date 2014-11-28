7efdd6034d846274712155b2dfafd5197ace7cba
icasdri <icasdri@gmail.com>
2014-11-27 21:47:14 -0500

    First steps towards an implementation based on a warning generator.

--- pybatterymonitor.py
+++ pybatterymonitor.py
@@ -31,46 +31,44 @@ UPOWER_PATH = "/org/freedesktop/UPower"
 UPOWER_IFACE = UPOWER_NAME
 DEV_IFACE = "org.freedesktop.UPower.Device"
 
-LOGIND_MANAGER_IFACE = "org.freedesktop.login1.Manager"
-LOGIND_SESSION_IFACE = "org.freedesktop.login1.Session"
-
 DEVICE_TYPES = {"Unknown": 0, "Line Power": 1, "Battery": 2}
+BATTERY_STATES = {0: "Unknown",
+                  1: "Charging",
+                  2: "Discharging",
+                  3: "Empty",
+                  4: "Fully Charged",
+                  5: "Pending Charge",
+                  6: "Pending Discharge"}
 
 class BatteryMonitor(dbus.service.Object):
-    def __init__(self, system_bus, session_bus):
+    def __init__(self, system_bus, session_bus, lower_bound=40, upper_bound=80, warn_step=5):
+        self.lower_bound = 40
+        self.upper_bound = 80
+        self.warn_step = 5
+
         self.system_bus = system_bus
         self.session_bus = session_bus
         bus_name = dbus.service.BusName(MY_IFACE, bus=self.session_bus)
         dbus.service.Object.__init__(self, bus_name, MY_PATH)
 
-        self.upower = dbus.Interface(
-            self.system_bus.get_object(UPOWER_NAME, UPOWER_PATH),
-            UPOWER_IFACE)
-
-        self.upower.connect_to_signal("PropertiesChanged",
-                                      self.handle_upower_signal,
-                                      dbus_interface=dbus.PROPERTIES_IFACE)
-        self.system_bus.add_signal_receiver(self.handle_prepare_for_sleep_signal,
-                                            signal_name="PrepareForSleep",
-                                            dbus_interface=LOGIND_MANAGER_IFACE)
-        self.system_bus.add_signal_receiver(self.handle_unlock_signal,
-                                            signal_name="Unlock",
-                                            dbus_interface=LOGIND_SESSION_IFACE)
         self.battery = None
-        self.percentage = None
         self.discharging = None
+        self.next_warning = None
+        self.warning_generator = None
         self.init_battery()
 
     def init_battery(self):
-        for dev_path in self.upower.EnumerateDevices():
+        upower = dbus.Interface(self.system_bus.get_object(UPOWER_NAME, UPOWER_PATH), UPOWER_IFACE)
+        for dev_path in upower.EnumerateDevices():
             dev_obj = self.system_bus.get_object(UPOWER_NAME, dev_path)
             dev_props = dbus.Interface(dev_obj, dbus.PROPERTIES_IFACE)
             if dev_props.Get(DEV_IFACE, "Type") == DEVICE_TYPES["Battery"] and \
                             dev_props.Get(DEV_IFACE, "PowerSupply") == True:
                 self.battery = dev_props
                 log.info("Found battery {} {}".format(dev_props.Get(DEV_IFACE, "Vendor"), dev_props.Get(DEV_IFACE, "Model")))
-                self.percentage = self.battery.Get(DEV_IFACE, "Percentage")
-                self.discharging =
+                self.update_percentage(self.battery.Get(DEV_IFACE, "Percentage"))
+                self.update_state(self.battery.Get(DEV_IFACE, "State"))
+                self.update_warnings()
                 self.battery.connect_to_signal("PropertiesChanged", self.handle_battery_signal)
                 break
         else:
@@ -78,46 +76,59 @@ class BatteryMonitor(dbus.service.Object):
 
     def handle_battery_signal(self, interface, data, signature):
         if "Percentage" in data:
-            log.info("Battery now at {} percent".format(data["Percentage"]))
+            self.update_percentage(data["Percentage"])
+        if "State" in data:
+            self.update_state(data["State"])
 
-    def handle_upower_signal(self, interface, data, signature):
-        if "OnBattery" in data:
-            if data["OnBattery"]:
-                log.info("Now running on Battery")
-            else:
-                log.info("Now running on Line Power")
-
-    def handle_unlock_signal(self):
-        log.info("A session has been unlocked")
-
-    def handle_prepare_for_sleep_signal(self, indicator):
-        if indicator == 0:
-            log.info("System is going for sleep!")
+    def new_warning_generator(self):
+        if self.discharging:
+            for i in range(self.lower_bound, 0, -self.warn_step):
+                yield (True, i)
+            yield (True, 0)
         else:
-            log.info("System has exited sleep")
+            for i in range(self.upper_bound, 100, self.warn_step):
+                yield (False, i)
+            yield (False, 100)
 
-    def handle_signal(self, *posargs, **kwargs):
-        #print("Positional Arguments")
-        for i, p in enumerate(posargs):
-            print("{}: {}".format(i, p))
-        #print("Keyword Arguments")
-        for k, a in kwargs.items():
-            print("{}: {}".format(k, a))
-        print()
+    def warn(self, percentage):
+        if self.discharging:
+            warn_string = "Battery is now at {} percent. Consider ending discharge."
+        else:
+            warn_string = "Battery is now at {} percent. Consider ending charge."
+        log.warning(warn_string)
 
-    def handle_device_added_signal(self):
-        pass
+    def update_percentage(self, new_percentage):
+        # If percentage and state matches that of next_warning, then warn,
+        # and pop a warning from the warning generator and put it at next_warning
+        # If next_warning is None, do nothing
+        log.info("Battery now at {} percent".format(new_percentage))
+
+    def update_state(self, new_state):
+        # If discharge/charge changes, make a new Warning Generator to match the change
+        if BATTERY_STATES[new_state] in ("Discharging", "Empty", "Pending Discharge"):
+            if not self.discharging:
+                self.discharging = True
+                self.update_warnings()
+        elif BATTERY_STATES[new_state] in ("Charging", "Fully Charged", "Pending Charge"):
+            if self.discharging:
+                self.discharging = False
+                self.update_warnings()
+        log.info("Battery is now {}".format(BATTERY_STATES[new_state]))
+
+    def update_warnings(self):
+        log.info("New warning set generated")
+        self.warning_generator = self.new_warning_generator()
+        self.next_warning = next(self.warning_generator, None)
 
     @dbus.service.method(dbus_interface=MY_IFACE)
     def Query(self):
-        pass
+        if self.battery is not None:
+            pass
 
     @dbus.service.method(dbus_interface=MY_IFACE)
     def NotifyQuery(self):
-        pass
-
-    def act_if_needed(self):
-        pass
+        if self.battery is not None:
+            pass
 
 if __name__ == "__main__":
     from dbus.mainloop.glib import DBusGMainLoop
