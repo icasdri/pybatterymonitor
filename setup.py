2a7535021dc550530c02a5a1bc4c0eb20e273ff3
icasdri <icasdri@gmail.com>
2014-11-29 23:03:23 -0500

    Assigned version 0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pybatterymonitor',
-    version='0.3',
+    version='0.1',
     license='GPL3',
     author='icasdri',
     author_email='icasdri@gmail.com',
